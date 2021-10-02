1ab3dc0937a18cd8e0c7a2b43158388708513889
theta405 <theta405@outlook.com>
2021-10-03 01:12:46 +0800

    细节优化，完善判断机制

--- commands/conf.py
+++ commands/conf.py
@@ -1,6 +1,6 @@
 #模块特殊操作
 
-from public import sendMsg, waitForReply
+from public import parseException, sendMsg, waitForReply
 onOffTables = {"y": True, "n": False, "Y": True, "N": False}
 actionsList = ["add", "remove"]
 conflictList = {"commands": ["指令", "c"], "monitors": ["监视", "m"]}
@@ -47,12 +47,12 @@ def getParser():
     onOffParser = customParser(add_help = False) #允许和禁用所用的父解析器
     onOffParser.add_argument("status", type = str, choices = onOffTables)
 
-    subparser = parser.add_subparsers(title = "可操作的选项", dest = "actionType") #添加子命令解析器，处理多参数的情况
+    subparser = parser.add_subparsers(title = "可操作的选项", dest = "hasParams") #添加子命令解析器，处理多参数的情况
     
     for k, v in onOffOptions.items():
-        subparser.add_parser(k, aliases = [v[0]], parents = [onOffParser], help = v[1])
+        subparser.add_parser(k, aliases = [v[0]], parents = [onOffParser], help = v[1]).set_defaults(actionType = k)
     for k, v in actionsOptions.items():
-        subparser.add_parser(k, aliases = [v[0]], parents = [actionParser], help = v[1])
+        subparser.add_parser(k, aliases = [v[0]], parents = [actionParser], help = v[1]).set_defaults(actionType = k)
 
     return parser
 
@@ -63,14 +63,14 @@ def execute(receive, sender, group, seq): #执行指令
 
     args = parser.parse_known_args(receive)
     name = args[0].name
-    checkConflict = hasConflict(name)
-    if not checkConflict[1]:
+    check = checkModule(name)
+    if not check[1]: #检查列表为空
         return "❌没有找到 {} 模块❌".format(name)
-    elif not checkConflict[0]: #检测模块是否被多次定义
-        return parseArgs(parser, args, getType(name)[0])
+    elif not check[0]: #检测模块是否被多次定义
+        return parseArgs(parser, args, check[1][0])
     else: #提示并等待输入
-        sendMsg(sender, group, "在以下位置存在多个 {} 模块：\n{}\n请手动指定模块".format(name, "、".join(["{} [{}]".format(_[0], _[1]) for _ in checkConflict[1]])))
-        sendMsg(sender, group, "例如：输入 {} 会指定为 {} 模块".format(checkConflict[1][0][1], checkConflict[1][0][0]))
+        sendMsg(sender, group, "在以下位置存在多个 {} 模块：\n{}\n请手动指定模块".format(name, "、".join(["{} [{}]".format(conflictList[_][0], conflictList[_][1]) for _ in check[1]])))
+        sendMsg(sender, group, "例如：输入 {} 会指定为 {} 模块".format(conflictList[check[1][0]][1], conflictList[check[1][0]][0]))
         while True: #判断输入是否有效
             choice = getTypeFromAbbr(waitForReply(properties.moduleName, seq, sender, group))
             if choice:
@@ -98,7 +98,12 @@ def getInformation(properties): #格式化输出权限
 
     return result + "\n\n{}：可使用  {}：不可使用".format(onSign, offSign)
 
-def getType(name):
+def getTypeFromAbbr(abbr): #根据缩写指定类型
+    for k, v in conflictList.items():
+        if abbr == v[1]:
+            return k
+
+def checkModule(name): #是否被多次定义
     checkList = []
     commands = getValue("commandList")
     monitors = getValue("monitorNames")
@@ -107,41 +112,21 @@ def getType(name):
         checkList.append("commands")
     if name in monitors:
         checkList.append("monitors")
-
-    return checkList
-
-def getTypeFromAbbr(abbr): #根据缩写指定类型
-    for k, v in conflictList.items():
-        if abbr == v[1]:
-            return k
-
-def hasConflict(name): #是否被多次定义
-    checkList = []
     
-    for i in getType(name):
-        checkList.append(conflictList[i])
-    
-    if len(checkList) >= 2:
-        return [True, checkList]
-    else:
-        return [False, checkList]
+    return [len(checkList) >= 2, checkList]
 
 def parseArgs(parser, args, moduleType): #解析参数
     def parse(): #解析后执行
         actionType = args[0].actionType
-        if not actionType in options: #从缩写获取完整的actionType
-            for k, v in options.items():
-                if actionType == v[0]:
-                    actionType = k
         permission = properties.getPermissions(actionType)
-        if actionType in onOffOptions:
+        if actionType in onOffOptions: #若为布尔型
             status = onOffTables[args[0].status]
             if status == permission:
                 result = "[ {} ] 已经为 {} ，无需修改".format(options[actionType][1], onSign if status else offSign)
             else:
                 properties.setPermission(actionType, status)
                 result = "已将 [ {} ] 设为 {}".format(options[actionType][1], onSign if status else offSign)
-        else:
+        else: #若为添加删除
             targetId = args[0].id
             if args[0].action == "add":
                 if targetId in permission:
@@ -158,7 +143,7 @@ def parseArgs(parser, args, moduleType): #解析参数
             properties.setPermission(actionType, permission)
         return result
 
-    commands = getValue("commandList")
+    commands = getValue("commandList") #必须保留，因为eval要用到
     monitors = getValue("monitorNames")
     onSign = getValue("onSign")
     offSign = getValue("offSign")
@@ -166,12 +151,16 @@ def parseArgs(parser, args, moduleType): #解析参数
     properties = eval(moduleType)[name].properties
     result = "执行结果：\n"
 
-    if not args[0].actionType:
+    if not args[0].hasParams: #若没有参数
         return getInformation(properties)
     else:
         while True:
             result += "> {}\n".format(parse())
             if not args[1]:
-                return result + "\n" + getInformation(properties)
-            args = parser.parse_known_args([name] + args[1])
-    
+                break
+            try: #防止中间有参数不对
+                args = parser.parse_known_args([name] + args[1])
+            except parseException as e:
+                result += "× 后续参数解析出错，原因如下：\n{}\n".format(str(e))
+                break
+        return result + "\n" + getInformation(properties)
