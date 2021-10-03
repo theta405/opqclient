7d1d0c95d772003ce3d66c418bad8b93404d6e76
theta405 <theta405@outlook.com>
2021-10-03 17:39:45 +0800

    提供了从命令行输入的功能，增加了等待连接的选项

--- client.py
+++ client.py
@@ -1,10 +1,15 @@
 from socketio import Client
 from public import getValue
+from time import sleep
+from threading import Thread
 
 sio = Client() #定义变量
 qq = getValue("qq")
+onSign = getValue("onSign")
+offSign = getValue("offSign")
+console = getValue("console")
 
-def parseMessage(message, sender, group = None):
+def parseMessage(message, sender, group = None, seq = None):
 	def moduleAvailable(properties): #检测模组是否可用
 		if (sender in properties["disabledUsers"]) or \
 			(group and group in properties["disabledGroups"]) or \
@@ -14,13 +19,18 @@ def parseMessage(message, sender, group = None):
 		return True
 
 	monitorList = getValue("monitorList") #每次执行时获取
-	data = message["CurrentPacket"]["Data"] #减少字典索引量
-	msgType = data["MsgType"] #获取消息类型
-	seq = data["MsgSeq"] #消息的唯一ID
-	if msgType in monitorList: #消息类型是否在监视列表内
-		for module in monitorList[msgType]: #遍历该类型消息下的所有监视模块
-			if moduleAvailable(module.properties.getPermissions()): #读取当前监视模块的设置
-				module.execute(data, sender, group, seq) #根据消息类型执行对应的监视模块
+
+	if sender == console: #检测是否是命令行输入
+		for module in monitorList["TextMsg"]: #遍历该类型消息下的所有监视模块
+			Thread(target = module.execute, args = ({"Content": message}, sender, group, seq)).start() #根据消息类型执行对应的监视模块
+	else:
+		data = message["CurrentPacket"]["Data"] #减少字典索引量
+		msgType = data["MsgType"] #获取消息类型
+		seq = data["MsgSeq"] #消息的唯一ID
+		if msgType in monitorList: #消息类型是否在监视列表内
+			for module in monitorList[msgType]: #遍历该类型消息下的所有监视模块
+				if moduleAvailable(module.properties.getPermissions()): #读取当前监视模块的设置
+					module.execute(data, sender, group, seq) #根据消息类型执行对应的监视模块
 
 @sio.on("OnGroupMsgs", namespace="/") #接收到群消息时
 def OnGroupMsgs(message):
@@ -41,12 +51,22 @@ def OnFriendMsgs(message):
 def OnEvents(message):
 	print(message)
 
-def main(): #连接OPQBot
-	try:
-		sio.connect(url = "http://localhost:{}".format(getValue("port")), transports = ["websocket"])
-		sio.wait()
-	except:
-		pass
+def connect(): #连接机器人
+	i = None
+	seq = 0
+	while True:
+		try: #尝试连接机器人
+			sio.connect(url = "http://localhost:{}".format(getValue("port")), transports = ["websocket"])
+			print("{}已连接QQ机器人".format("" if i == None else "\n"))
+			while True:
+				parseMessage(input("\r> "), console, seq = seq) #命令行输入
+				seq += 1
+		except KeyboardInterrupt: #若从控制台终止
+			exit()
+		except: #若连接失败则等待，同时在控制台输出文本
+			i = not i
+			print("\r正在连接QQ机器人 {}\r".format(onSign if i else offSign), end = "")
+			sleep(1)
 
 if __name__ == "__main__":
-	main()
+	connect()
